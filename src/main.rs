1ee99493a44a8e3ae581dc66b512ffdb37d8c29f
Nic Hartley <nxh9052@rit.edu>
2019-09-25 20:33:10 -0400

    it load file

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,16 @@
 use std::{
     collections::HashMap,
     env,
-    io::{BufRead, BufReader, Write as _},
+    io::{self, Lines, BufRead, BufReader, Write as _},
     net,
     path::{Path, PathBuf},
+    fs::File,
     time::Instant,
 };
 
 fn get_args() -> Result<(PathBuf, String, Vec<(String, String)>), String> {
     let mut args = env::args().skip(1);
-    let dir = Path::new(&args.next().unwrap_or(".".into()))
-        .canonicalize()
-        .expect("invalid path");
+    let dir = Path::new(&args.next().unwrap_or(".".into())).to_path_buf();
     let host = args.next().unwrap_or("localhost:8080".into());
     let mut mappings = HashMap::new();
     mappings.insert(".html".into(), "text/html".into());
@@ -32,14 +31,13 @@ fn get_args() -> Result<(PathBuf, String, Vec<(String, String)>), String> {
     Ok((dir, host, mappings))
 }
 
-fn get_path(input: &mut dyn BufRead) -> String {
-    let mut first_line = String::new();
-    input
-        .read_line(&mut first_line)
-        .expect("failed to read from TCP");
+fn get_path<B: BufRead>(input: &mut Lines<B>) -> String {
+    let first_line = input.next().expect("pipe ended early").expect("failed to read from TCP");
+    println!("First line: '{}'", first_line);
     let url_start = first_line.find(" ").expect("no space") + 1;
     let url_length = first_line[url_start..].find(" ").expect("no second space");
-    first_line[url_start..url_start + url_length].to_owned()
+    // + 1 to remove slash
+    first_line[url_start + 1..url_start + url_length].to_owned()
 }
 
 fn main() {
@@ -73,27 +71,33 @@ fn main() {
             Err(_) => continue,
         };
         // TODO: Read through request to get url + headers
-        let mut input = BufReader::new(&mut conn);
+        let mut input = BufReader::new(&mut conn).lines();
         let url = get_path(&mut input);
-        for line in input.lines() {
+        let filepath = dir.join(&url);
+        println!("request is for {:?} = {:?}", url, filepath);
+        // let content_types;
+        for line in input {
             let line = line.expect("failed to read from TCP");
             if line == "" {
                 break;
             }
             // println!("Header: {}", line);
         }
+        let mut doc = File::open(filepath).expect("failed to open file");
+        let metadata = doc.metadata().expect("failed to read metadata");
         // TOOD: Send response based on url, relevant headers
         write!(
             conn,
             concat!(
                 "HTTP/1.1 200 OK\n",
                 "Content-Type: text/plain\n",
-                "Content-Length: 13\n",
+                "Content-Length: {len}\n",
                 "\n",
-                "Hello, World!",
-            )
+            ),
+            len = metadata.len(),
         )
         .expect("failed to write response");
+        io::copy(&mut doc, &mut conn).expect("failed to write to stream");
         conn.flush().expect("failed to flush response");
         let resp_time = Instant::now().duration_since(resp_start);
         println!("Served {} in {}us", url, resp_time.as_micros());
