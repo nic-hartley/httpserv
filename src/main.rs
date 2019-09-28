69a68ad59f8607f62bed6fa41ce3a76022a1eda1
Nic Hartley <nxh9052@rit.edu>
2019-09-28 18:28:04 -0400

    yet more error handling, extension mapping, index.html finding

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::{
     collections::HashMap,
-    env,
-    io::{self, Read, Lines, BufRead, BufReader, BufWriter, Write as _},
-    net,
-    fmt,
-    path::{Path, PathBuf},
+    env, fmt,
     fs::File,
+    io::{self, BufRead, BufReader, BufWriter, Lines, Read, Write as _},
+    net,
+    path::{Path, PathBuf},
     time::Instant,
+    ffi::OsString,
 };
 
 #[derive(Debug)]
@@ -26,34 +26,43 @@ impl fmt::Display for Failure {
     }
 }
 
-fn get_args() -> Result<(PathBuf, String, Vec<(String, String)>), Failure> {
+fn get_args() -> Result<(PathBuf, String, HashMap<OsString, String>), Failure> {
     let mut args = env::args().skip(1);
     let dir = Path::new(&args.next().unwrap_or(".".into())).to_path_buf();
     let host = args.next().unwrap_or("localhost:8080".into());
     let mut mappings = HashMap::new();
-    mappings.insert(".html".into(), "text/html".into());
-    mappings.insert(".css".into(), "text/css".into());
-    mappings.insert(".js".into(), "text/javascript".into());
-    mappings.insert(".png".into(), "image/png".into());
-    mappings.insert(".jpg".into(), "image/jpeg".into());
-    mappings.insert(".jpeg".into(), "image/jpeg".into());
-    mappings.insert(".ico".into(), "image/vnd.microsoft.icon".into());
-    mappings.insert(".svg".into(), "image/svg+xml".into());
+    mappings.insert("html".into(), "text/html".into());
+    mappings.insert("css".into(), "text/css".into());
+    mappings.insert("js".into(), "text/javascript".into());
+    mappings.insert("png".into(), "image/png".into());
+    mappings.insert("jpg".into(), "image/jpeg".into());
+    mappings.insert("jpeg".into(), "image/jpeg".into());
+    mappings.insert("ico".into(), "image/vnd.microsoft.icon".into());
+    mappings.insert("svg".into(), "image/svg+xml".into());
     for pair in args {
-        let eq_idx = pair.find("=").ok_or_else(|| Failure::InvalidFormat(pair.clone()))?;
+        let eq_idx = pair
+            .find("=")
+            .ok_or_else(|| Failure::InvalidFormat(pair.clone()))?;
         let (ext, mime) = pair.split_at(eq_idx);
         mappings.insert(ext.into(), mime.into());
     }
-    let mappings = mappings.into_iter().collect();
 
     Ok((dir, host, mappings))
 }
 
 fn get_path<B: BufRead>(input: &mut Lines<B>) -> Result<String, Failure> {
-    let first_line = input.next().ok_or(Failure::EarlyInputEnd)?.or(Err(Failure::EarlyInputEnd))?;
+    let first_line = input
+        .next()
+        .ok_or(Failure::EarlyInputEnd)?
+        .or(Err(Failure::EarlyInputEnd))?;
     // +1 because we actually care about the next character, not this one
-    let url_start = first_line.find(" ").ok_or_else(|| Failure::InvalidFormat(first_line.clone()))? + 1;
-    let url_length = first_line[url_start..].find(" ").ok_or_else(|| Failure::InvalidFormat(first_line.clone()))?;
+    let url_start = first_line
+        .find(" ")
+        .ok_or_else(|| Failure::InvalidFormat(first_line.clone()))?
+        + 1;
+    let url_length = first_line[url_start..]
+        .find(" ")
+        .ok_or_else(|| Failure::InvalidFormat(first_line.clone()))?;
     Ok(first_line[url_start..url_start + url_length].to_owned())
 }
 
@@ -78,61 +87,88 @@ impl Response {
     }
 }
 
-fn get_response(filepath: &Path) -> Response {
+fn get_response(filepath: &Path, mappings: &HashMap<OsString, String>) -> Response {
     // TOOD: Send response based on url, relevant headers
-    let mut doc = match File::open(filepath) {
+    // TODO: More robust extension checking + checking for match with Accept header
+    let (filepath, ext) = match filepath.extension() {
+        Some(e) => (filepath.into(), e.to_owned()),
+        None => {
+            (filepath.join("index.html"), "html".into())
+        }
+    };
+    let mapped_type = match mappings.get(&ext) {
+        Some(t) => t.clone(),
+        None => "text/plain".into(),
+    };
+    let doc = match File::open(filepath) {
         Ok(d) => d,
         Err(e) => match e.kind() {
             io::ErrorKind::NotFound => return Response::NotFound,
             o => return Response::Error(format!("{:?}", o)),
-        }
+        },
     };
-    let metadata = doc.metadata().expect("failed to read metadata for real file");
+    let metadata = doc
+        .metadata()
+        .expect("failed to read metadata for real file");
     Response::Ok {
         headers: vec![],
-        body_type: "text/plain".into(),
+        body_type: mapped_type,
         body_len: metadata.len(),
         body: Box::new(doc),
     }
 }
 
-fn write_response(mut conn: net::TcpStream, response: Response) {
+fn write_response(conn: net::TcpStream, response: Response) -> io::Result<()> {
     let mut bufout = BufWriter::new(conn);
     match response {
-        Response::Ok { headers, body_type, body_len, mut body } => {
-            write!(bufout, "HTTP/1.1 200 OK\n").expect("failed to write to stream");
-            write!(bufout, "Content-Type: {}\n", body_type).expect("failed to write to stream");
-            write!(bufout, "Content-Length: {}\n", body_len).expect("failed to write to stream");
-            write!(bufout, "Connection: close\n",).expect("failed to write to stream");
+        Response::Ok {
+            headers,
+            body_type,
+            body_len,
+            mut body,
+        } => {
+            write!(bufout, "HTTP/1.1 200 OK\n")?;
+            write!(bufout, "Content-Type: {}\n", body_type)?;
+            write!(bufout, "Content-Length: {}\n", body_len)?;
+            write!(bufout, "Connection: close\n",)?;
             for (name, val) in headers {
-                write!(bufout, "{}: {}\n", name, val).expect("failed to write header to stream");
+                write!(bufout, "{}: {}\n", name, val)?;
             }
-            write!(bufout, "\n").expect("failed to write separator to stream");
-            io::copy(&mut body, &mut bufout).expect("failed to write body to stream");
+            write!(bufout, "\n")?;
+            io::copy(&mut body, &mut bufout)?;
         }
         Response::NotFound => {
-            write!(bufout, concat!(
-                "HTTP/1.1 404 Not Found\n",
-                "Content-Length: 0\n",
-                "Connection: close\n",
-                "\n"
-            )).expect("failed to write error to stream");
+            write!(
+                bufout,
+                concat!(
+                    "HTTP/1.1 404 Not Found\n",
+                    "Content-Length: 0\n",
+                    "Connection: close\n",
+                    "\n"
+                )
+            )?;
         }
         Response::Error(msg) => {
-            write!(bufout, concat!(
-                "HTTP/1.1 404 Not Found\n",
-                "Content-Length: {len}\n",
-                "Connection: close\n",
-                "\n",
-                "{msg}"
-            ), len = msg.len(), msg = msg).expect("failed to write error to stream");
+            write!(
+                bufout,
+                concat!(
+                    "HTTP/1.1 404 Not Found\n",
+                    "Content-Length: {len}\n",
+                    "Connection: close\n",
+                    "\n",
+                    "{msg}"
+                ),
+                len = msg.len(),
+                msg = msg
+            )?;
         }
-    }
+    };
+    Ok(())
 }
 
 fn main() {
     let load_start = Instant::now();
-    let (dir, host, _mappings) = match get_args() {
+    let (dir, host, mappings) = match get_args() {
         Ok(t) => t,
         Err(msg) => {
             println!("Invalid command: {:?}", msg);
@@ -142,7 +178,7 @@ fn main() {
     let listener = match net::TcpListener::bind(&host) {
         Ok(l) => l,
         Err(e) => {
-            eprintln!("Failed to listen where told: {:?}", e);
+            eprintln!("Failed to listen on {}: {:?}", host, e);
             return;
         }
     };
@@ -164,7 +200,10 @@ fn main() {
         let mut input = BufReader::new(&mut conn).lines();
         let url = match get_path(&mut input) {
             Ok(u) => u,
-            Err(e) => { println!("Failed to get path: {:?}", e); continue; },
+            Err(e) => {
+                println!("Failed to get path: {:?}", e);
+                continue;
+            }
         };
         let filepath = dir.join(&url[1..]);
         // let content_types;
@@ -175,10 +214,18 @@ fn main() {
             }
             // println!("Header: {}", line);
         }
-        let response = get_response(&filepath);
+        let response = get_response(&filepath, &mappings);
         let code = response.code();
-        write_response(conn, response);
+        if let Err(e) = write_response(conn, response) {
+            println!("Failed to write to pipe: {:?}", e);
+            continue;
+        }
         let resp_time = Instant::now().duration_since(resp_start);
-        println!("Served {} with {} in {}ms", url, code, resp_time.as_millis());
+        println!(
+            "Served {} with {} in {}ms",
+            url,
+            code,
+            resp_time.as_millis()
+        );
     }
 }
